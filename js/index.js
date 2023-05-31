37e2d1ff6843cd75ed43ddeeedd45934aefd608c
753x <133068862+753x@users.noreply.github.com>
2023-05-31 12:42:35 +0900

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,24 @@
+﻿/* 메뉴 슬라이드 토글 */
+$(function(){
+	$('.hamburger').on('click', function(){
+		$('nav').slideToggle();
+		$(this).toggleClass('is-active');
+	});
+});
+
+/* 메뉴 슬라이드 토글 끝 */
+
+
+/* 사진 마우스 오버시 예약하기 버튼과 간단한 설명 */
+
+$(function(){
+	$('.theme-main img').on('mouseenter', function(){
+		$(this).siblings('.info').fadeIn();
+	});
+	$('.theme-main .img-wrapper').on('mouseleave', function(){
+		$(this).find('.info').fadeOut();
+	});
+});
+	
+
+	
